b83f40a563407fcf9211eddffd911a1a5b3e7079
drezr <ultimove@gmail.com>
2021-12-07 20:18:48 +0100

    +

---
+++ src/views/watcher/components/LeaveType.vue
@@ -0,0 +1,56 @@
+<template>
+
+<div>
+
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+  name: 'LeaveType',
+
+  components: {
+    Profile,
+  },
+
+  props: {
+
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  created() {
+
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+  },
+
+  watch: {
+
+  },
+}
+
+</script>
+
+
+<style>
+
+</style>
+
+
+<style scoped>
+
+</style>
