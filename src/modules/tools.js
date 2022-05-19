508b7014ef2f4193840b202e8e9b3494547a377d
drezr <ultimove@gmail.com>
2022-05-19 14:19:52 +0200

    Fixed getWeek function

--- src/modules/tools.js
+++ src/modules/tools.js
@@ -2,9 +2,15 @@ const uuidv4 = require('uuid/v4')
 
 
 Date.prototype.getWeek = function() {
-    let startDate = new Date(this.getFullYear(), 0, 1)
-    var days = Math.floor((this - startDate) / (24 * 60 * 60 * 1000))
-    return Math.ceil((this.getDay() + 1 + days) / 7) - 1
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
 }
 
 function drag_dialog() {
